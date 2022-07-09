a4eff0c90d7f4053c8f00940c71c4b3cb4647274
devnaveenkumar <98394930+devnaveenkumar@users.noreply.github.com>
2022-07-09 19:42:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var home=document.getElementById("home");
+home.onclick=function home(){
+    window.location.href="index.html";
+}
+var search=document.getElementById("search");
+search.onclick=function search(){
+    location.href="search.html";
+}
+var playlist =document.getElementById("cp");
+cp.onclick=function playlist(){
+    location.href="";
+}
+var library=document.getElementById("lib");
+library.onclick=function library(){
+    location.href="";
+}
+var likedsong=document.getElementById("lk");
+likedsong.onclick=function song(){
+    location.href="";
+}
